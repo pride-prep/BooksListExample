2a797a17a21fdfe8957fc50f728ac07f2bf00dfd
Tucker Boniface <tucker@my.innovationspokane.org>
2018-07-10 20:19:13 -0700

    swiftformat linting

--- BooksList/BooksList/library.swift
+++ BooksList/BooksList/library.swift
@@ -10,9 +10,9 @@ import Foundation
 
 // function for searching books under certain criteria based on user input
 func searchBooks() {
-    var isFinished = false;
-    var input: String;
-    var menuOption: Int;
+    var isFinished = false
+    var input: String
+    var menuOption: Int
     // let user access submenu until they wish to exit
     while !isFinished {
         // print submenu
@@ -28,23 +28,23 @@ func searchBooks() {
         if Double(input) != nil {
             menuOption = Int(input)!
             // search the books by criteria
-            switch(menuOption) {
+            switch menuOption {
             case 1: // prompt user for author name
-                print("Enter author name: ", terminator:"")
+                print("Enter author name: ", terminator: "")
                 input = readLine()!
                 searchBooksByCriteria(criteria: "author", data: input)
             case 2: // prompt user for book name
-                print("Enter book name: ", terminator:"")
+                print("Enter book name: ", terminator: "")
                 input = readLine()!
                 searchBooksByCriteria(criteria: "name", data: input)
             case 3: // return list of books that are in stock
                 searchBooksByCriteria(criteria: "inStock", data: "")
             case 4:
                 // we will need to get the min and max range of the price search
-                var range: (Double, Double);
-                print("Enter min price range: ", terminator:"")
+                var range: (Double, Double)
+                print("Enter min price range: ", terminator: "")
                 range.0 = Double(readLine()!)!
-                print("Enter max price range: ", terminator:"")
+                print("Enter max price range: ", terminator: "")
                 range.1 = Double(readLine()!)!
                 searchBooksByCriteria(criteria: "range", data: range)
             case 5: isFinished = true // return back to main menu
@@ -59,13 +59,13 @@ func searchBooks() {
 
 // search for books based on criteria
 func searchBooksByCriteria(criteria: String, data: Any) {
-    var count: Int = 0; // count used as flag for number of books in stock and price range
+    var count: Int = 0 // count used as flag for number of books in stock and price range
     // if books are in stock
     if criteria == "inStock" {
         for book in books {
             if book.inStock {
                 print(book.getInfo())
-                count += 1;
+                count += 1
             }
         }
         if count == 0 {
@@ -103,4 +103,3 @@ func searchBooksByCriteria(criteria: String, data: Any) {
     // output no results if no results are found in for loop searches
     print("No results for \(data)")
 }
-
