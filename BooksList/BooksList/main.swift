7bb95215fa10c85a83d9681734d11a11c2d3f11d
Tucker Boniface <tucker@my.innovationspokane.org>
2018-07-10 20:19:13 -0700

    swiftformat linting

--- BooksList/BooksList/main.swift
+++ BooksList/BooksList/main.swift
@@ -8,33 +8,34 @@
 
 import Foundation
 
-/*///////////////////////////////
+/* ///////////////////////////////
  Pre-load the data
- *///////////////////////////////
+ */ //////////////////////////////
 // Create an empty array of books
-var books: [Book] = [];
+var books: [Book] = []
 
 // Fill our array with all of the books in the book database
 for book in bookData {
-    var newBook: Book = Book(); // create a new book object
+    var newBook: Book = Book() // create a new book object
     // convert the dictionary format to object
     for (key, value) in book {
-        newBook.convertDictToObj(key: key, value: value);
+        newBook.convertDictToObj(key: key, value: value)
     }
     // push the object to the array
-    books.append(newBook);
+    books.append(newBook)
 }
-/*///////////////////////////////
+
+/* ///////////////////////////////
  End of pre-load data
- *///////////////////////////////
+ */ //////////////////////////////
 
-/*///////////////////////////////
+/* ///////////////////////////////
  Start of Interface
- *///////////////////////////////
+ */ //////////////////////////////
 
-var isFinished = false; // continue interface until users opts out
-var input: String; // container for input
-var menuOption: Int; // variable for options
+var isFinished = false // continue interface until users opts out
+var input: String // container for input
+var menuOption: Int // variable for options
 
 print("Welcome to the PRIDE Prep Library!")
 
@@ -44,22 +45,22 @@ while !isFinished {
     print("\t1) Search for a book")
     print("\t2) Display all library books")
     print("\t3) I'm done, log me out.")
-    print("Option: ", terminator:"")
-    input = readLine()!;
+    print("Option: ", terminator: "")
+    input = readLine()!
     // make sure that the user input is only a number
     if Double(input) != nil {
         menuOption = Int(input)!
         // ensure user only selects options 1-3
-        switch(menuOption) {
+        switch menuOption {
         case 1: searchBooks() // search books
         case 2: // show all books
             for book in books {
                 print(book.getInfo())
             }
         case 3: // exit program
-            isFinished = true;
+            isFinished = true
             print("Cya later!")
-        default: print("Not a valid option");
+        default: print("Not a valid option")
         }
     } else {
         print("Please enter a valid number")
